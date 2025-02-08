0b67042b7b3ea036e1f1b097f77ce428a211a893
alvindioprakosa <alvindioprakosa@gmail.com>
2025-02-08 10:45:36 +0700

    Update Dashboard Layout

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -6,47 +6,47 @@ import Header from "../components/web/Header";
 import Sidebar from "../components/web/Sidebar";
 
 const LayoutDashboard = ({ children }) => {
-	//state user
-	const setUser = useState({});
-	const history = useHistory();
-	const token = Cookies.get("token");
+  //state user
+  const setUser = useState({});
+  const history = useHistory();
+  const token = Cookies.get("token");
 
-	//fetchData
-	const fetchData = async () => {
-		//fetch on Rest API
-		await Api.get("/profile", {
-			headers: {
-				//header Bearer + Token
-				Authorization: `Bearer ${token}`,
-			},
-		}).then((response) => {
-			//set state "user"
-			if(response.data.email_verified_at === null){
-				history.push("/verify-email");
-			}
-			// else{
-			// 	setUser(response.data);
-			// }
-		});
-	};
+  //fetchData
+  const fetchData = async () => {
+    //fetch on Rest API
+    await Api.get("/profile", {
+      headers: {
+        //header Bearer + Token
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => {
+      //set state "user"
+      if (response.data.email_verified_at === null) {
+        history.push("/verify-email");
+      }
+      // else{
+      // 	setUser(response.data);
+      // }
+    });
+  };
 
-	//hook useEffect
-	useEffect(() => {
-		//call function "fetchData"
-		fetchData();
+  //hook useEffect
+  useEffect(() => {
+    //call function "fetchData"
+    fetchData();
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	return (
-		<React.Fragment>
-			<Header />
-			<div className="px-10 mt-14 md:px-24 flex flex-col md:flex-row gap-x-4 items-start">
-				<Sidebar />
-				{children}
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="px-10 mt-14 md:px-24 flex flex-col md:flex-row gap-x-4 items-start">
+        <Sidebar />
+        {children}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default LayoutDashboard;
