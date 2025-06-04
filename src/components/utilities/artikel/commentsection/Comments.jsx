4083d4bc4e70d5deafb6f50938fbd25036f1a94e
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-04 09:17:21 +0700

    Update Comment

--- src/components/utilities/artikel/commentsection/Comments.jsx
+++ src/components/utilities/artikel/commentsection/Comments.jsx
@@ -3,14 +3,18 @@ import Comment from "./Comment";
 // import CommentForm from "./CommentForm";
 
 function Comments({ comments, fetchDataBlog }) {
-	return (
-		<div>
-			{comments.map((comment) => (
-				<Comment key={comment.id} comment={comment} fetchDataBlog={fetchDataBlog} />
-			))}
-			{/* <CommentForm /> */}
-		</div>
-	);
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment}
+          fetchDataBlog={fetchDataBlog}
+        />
+      ))}
+      {/* <CommentForm /> */}
+    </div>
+  );
 }
 
 export default Comments;
